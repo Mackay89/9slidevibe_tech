589a911a99bc5670d33d89355eae0d8437596a02
khaya <122832928+Mackay89@users.noreply.github.com>
2024-10-17 12:39:38 +0200

    Delete ecommerce-backend directory

--- ecommerce-backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login } = require('../controllers/authController'); // ensure this is correct
-
-router.post('/login', login); // this should be defined and not undefined
-
-module.exports = router;
-
